d524123612eca4c9c57b849d506e659a4df07207
Wesley Mendes <wesleymendes123321@gmail.com>
2023-05-02 21:30:01 -0300

    exercicios essenciais feitos

---
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/exercicio8.js
@@ -0,0 +1,13 @@
+let primeiro = 3;
+let segundo = 3;
+let terceiro = 3;
+
+let par = true;
+
+if (primeiro%2 === 0 || segundo%2 === 0 || terceiro%2 === 0) {
+    par = true;
+} else {
+    par = false;
+}
+
+console.log(par);
