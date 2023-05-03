c477a63a74fbb4a6c18bb833bb00d8caebefc6b9
Wesley Mendes <wesleymendes123321@gmail.com>
2023-05-02 21:03:48 -0300

    adição exercicios conteudo

---
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/conteudo2.js
@@ -0,0 +1,6 @@
+let base = 5;
+let height = 8;
+area = base * height;
+
+console.log(area);
+
---
+++ 1. Fundamentos/3. Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/exercicio2.js
@@ -0,0 +1,8 @@
+let um = 20;
+let dois = 30;
+
+if (um > dois) {
+    console.log("variavel um é maior");
+} else {
+    console.log("variável dois é maior");
+}
